46fade15d604cdd20821cf7aee138102040ff43a
A1z6n <152766056+A1z6n@users.noreply.github.com>
2025-03-14 23:36:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// Exports the "silver" theme for usage with module loaders
+// Exports the "help" plugin for usage with module loaders
 // Usage:
 //   CommonJS:
-//     require('tinymce/themes/silver')
+//     require('tinymce/plugins/help')
 //   ES2015:
-//     import 'tinymce/themes/silver'
-require('./theme.js');
+//     import 'tinymce/plugins/help'
+require('./plugin.js');
